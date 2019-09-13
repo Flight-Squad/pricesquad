49b7a4ba72c3f6b10f7f2204c75e559557cc10ea
Pujit Mehrotra <pujitm2009@gmail.com>
2019-09-13 15:09:22 -0400

    Add skiplagged and googleflights scraper modules

---
+++ googleFlights.js
@@ -0,0 +1,76 @@
+import cheerio from 'cheerio'
+import Nightmare from 'nightmare'
+
+export async function googleFlights() {
+  const processStartTime = process.hrtime()
+  const nightmare = Nightmare()
+  const url = "https://www.google.com/flights?hl=en#flt=/m/01cx_.r/m/059rby.2019-09-29*r/m/059rby./m/01cx_.2019-10-03;c:USD;e:1;ls:1w;sd:1;t:e"
+  const sel = 'div[class*="card"]';
+
+  const listContainer = await nightmare
+    .goto(url)
+    .wait('ol')
+    .evaluate(() => document.querySelector('ol').innerHTML)
+    .end();
+
+    const prices = [];
+    const scraper = cheerio.load(listContainer);
+    scraper('span[class*="price"]').each(function (i, elem) {
+      prices.push(scraper(this).text());
+    })
+
+    const stops = [];
+    scraper('span[class*="gws-flights__ellipsize"]').each(function (i, elem) {
+      stops.push(scraper(this).text());
+    })
+
+    const durations = [];
+    scraper('span[class*="duration"]').each(function (i, elem) {
+      durations.push(scraper(this).text());
+    })
+
+    const airlines = [];
+    scraper('img[class*="airline-logo"]').each(function (i, elem) {
+      airlines.push(scraper(this).attr('alt'));
+    })
+
+    // Everything has same number of elements
+    const dataIsConsistent = prices.length === stops.length && stops.length === durations.length && stops.length === airlines.length;
+    const trips = [];
+    if (dataIsConsistent) {
+      for (let i = 0; i < stops.length; i ++) {
+        trips.push({
+          price: prices[i],
+          stops: stops[i],
+          airline: airlines[i],
+          duration: durations[i],
+        })
+      }
+    } else {
+      // Warn and put it in a try catch and return what you can
+      console.log('DATA NOT CONSISTENT');
+    }
+
+    const processEndTime = process.hrtime(processStartTime);
+    console.log(`GoogleFlights: ${processEndTime[0]}s ${processEndTime[1]}nanos`);
+
+    return trips;
+
+    // Keeping this for future demos
+
+    // console.log('====================================');
+    // console.log(prices);
+    // console.log(stops);
+    // console.log(durations);
+    // console.log(airlines);
+    // console.log(prices.length === stops.length && stops.length === durations.length && stops.length === airlines.length);
+
+
+    // console.log('====================================');
+
+    // console.log('====================================');
+    // console.log(listContainer);
+    // console.log('====================================');
+}
+
+googleFlights().then(res => console.log(res));
---
+++ skiplagged.js
@@ -0,0 +1,91 @@
+import axios from 'axios'
+import cheerio from 'cheerio'
+import Nightmare from 'nightmare'
+
+// Auto sorts by cheapest price, so do we really need to keep scrolling?
+// Maybe for flight from cheapest provider
+// If provider isn't present in top 10 list, scroll
+export async function skiplaggedFlights() {
+  const processStartTime = process.hrtime()
+  const nightmare = Nightmare()
+  const url = "https://skiplagged.com/flights/NYC/SFO/2019-09-27/2019-10-20"
+
+  // TODO Scroll to bottom of page
+  // https://github.com/segmentio/nightmare/issues/625#issuecomment-217730846
+  // very naive method - will infinite loop on a truly infinite scrolling page
+
+  const listContainer = await nightmare
+    .goto(url)
+    .wait('.trip-duration')
+    .evaluate(() => document.querySelector('.trip-list').innerHTML)
+    .end();
+
+  const scraper = cheerio.load(listContainer);
+  const totalTrips = scraper('.trip').length;
+  const tripsData = [];
+  scraper('.trip').each(async function (i, elem) {
+    const trip = {};
+
+    // the text selector in this call returns all of the plain text
+    // under the div we've selected, resulting in a string like "11h1 stop"
+    let durationAndStops = scraper(this).find('.trip-duration').text();
+
+    // This is a hacky workaround solution
+    // the text selector in the previous call returns all of the plain text
+    // under the div we've selected, resulting in a string like "11h1 stop"
+    // This split calls splits the text into hours (first elem) and a string
+    // indicating number of stops (second elem)
+    durationAndStops = durationAndStops.split("h");
+    trip.duration = durationAndStops[0];
+    trip.stops = durationAndStops[1];
+
+    const airlinesData = scraper(this).find('.airlines').data('original-title');
+    trip.airlineNumbers = getAirlineNumbers(airlinesData);
+
+    trip.price = scraper(this).find('.trip-cost').find('p').text();
+
+    // TODO trip path
+
+
+    // console.log('====================================');
+    // console.log(airlineNumbers);
+    // console.log(price)
+    // console.log('====================================');
+
+    // TODO
+    // If everything is valid, return
+    // If not, log error/missing components, move onto next element. Do NOT add to list of return objects
+    if (trip.price) {
+      tripsData.push(trip);
+    } else {
+      // Log
+    }
+  })
+  const processEndTime = process.hrtime(processStartTime);
+  console.log(`skiplaggedFlights: ${processEndTime[0]}s ${processEndTime[1]}nanos`);
+  return tripsData;
+
+  // const trips = [];
+  // console.log('====================================');
+  // console.log(scraper('.trip').length);
+  // console.log('====================================');
+}
+
+function getAirlineNumbers(airlineData) {
+  // bug here --> airline data undef
+  if (!airlineData) return;
+
+  const airlineNumbers = [];
+  try {
+    const airScraper = cheerio.load(airlineData);
+    airScraper('span').each(function (i, elem) {
+      airlineNumbers.push(airScraper(this).text());
+    })
+  } catch (err) {
+    console.log('====================================');
+    console.log("ERROR");
+    console.log(airlineData);
+    console.log('====================================');
+  }
+  return airlineNumbers;
+}
