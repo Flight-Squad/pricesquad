3911910673543f09b9bd963da8aa29aef71e3a18
Pujit Mehrotra <pujitm2009@gmail.com>
2019-10-08 15:39:59 -0400

    Comment out providers not implemented by scraper

--- src/data/models/flightSearch/providers.ts
+++ src/data/models/flightSearch/providers.ts
@@ -1,6 +1,7 @@
 // Essentially syntactic sugar for JS object -> https://stackoverflow.com/a/39372911/6656631
 export enum SearchProviders {
   google = 'google',
-  kayak = 'kayak',
-  southwest = 'southwest',
+  // TODO NOT IMPLEMENTED
+  // kayak = 'kayak',
+  // southwest = 'southwest',
 }
