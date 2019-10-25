7a48fcba90e69a14a4d865d55f17db63d7cdad61
Pujit Mehrotra <pujitm2009@gmail.com>
2019-10-25 16:55:25 -0400

    Get entire config from single env var

--- src/config/firestore.ts
+++ src/config/firestore.ts
@@ -2,18 +2,7 @@ import admin from 'firebase-admin';
 import path from 'path';
 const fs = require('fs');
 
-const creds = JSON.stringify({
-  "type": process.env.FS_ACCOUNT_TYPE,
-  "project_id": process.env.FS_PROJ_ID,
-  "private_key_id": process.env.FS_PRIV_KEY_ID,
-  "private_key": process.env.FS_PRIV_KEY,
-  "client_email": process.env.FS_CLIENT_EMAIL,
-  "client_id": process.env.FS_CLIENT_ID,
-  "auth_uri": process.env.FS_AUTH_URI,
-  "token_uri": process.env.FS_TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.FS_AUTH_CERT_URL,
-  "client_x509_cert_url": process.env.FS_CLIENT_CERT_URL,
-});
+const creds = process.env.FS_CONFIG;
 
 // write to a new file named 2pac.txt
 fs.writeFileSync(path.resolve(__dirname, './serviceAccount.json'), creds);
