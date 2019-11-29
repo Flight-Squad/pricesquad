7b3198650e7d1573f577019e9d118feff67d3d53
Pujit Mehrotra <pujitm2009@gmail.com>
2019-11-29 02:31:45 -0500

    Add trigger to show prices

--- src/api/tripRequests.ts
+++ src/api/tripRequests.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import StatusCodes from "./config/statusCodes";
 import { db } from "config/firestore";
+import axios from 'axios';
 
 const requestsRouter = Router();
 
@@ -9,8 +10,21 @@ requestsRouter.post('/tripRequest', async (req, res) => {
     [req.body.provider]: req.body.results,
   };
 
-  await db.collection('trip_requests').doc(`${req.body.sessionId}#${req.body.requestId}`).set(docData, {merge: true});
+  const docId = `${req.body.sessionId}#${req.body.requestId}`;
+
+  await db.collection('trip_requests').doc(docId).set(docData, {merge: true});
+  if (await triggerShowPrices(docId)) {
+    axios.post(`${process.env.CHATSQUAD_API}/sendPrices`, {
+      sessionId: req.body.sessionId,
+      requestId: req.body.requestId,
+    });
+  }
   res.status(StatusCodes.Post.success).send();
 });
+
+async function triggerShowPrices(docId) {
+  // Check if all data is present
+  return true;
+}
 
 export default requestsRouter;
