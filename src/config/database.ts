0399d65721203bb868613f42805dc38b96437e1e
Pujit Mehrotra <pujitm2009@gmail.com>
2020-02-07 14:53:08 -0500

    Update admin to 0.0.49

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,15 @@
 import { Database, AirportLocMap } from '@flight-squad/admin';
 import path from 'path';
 
+console.log('init');
+
 Database.init({
     serviceAccount: process.env.FIRESTORE_CONFIG,
     firebaseUrl: process.env.FIREBASE_URL,
     serviceAccountPath: path.resolve(__dirname, './serviceAccount.json'),
 });
+
+console.log('done');
 
 export const DB = Database.firebase;
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 import 'module-alias/register'; // Register absolute import paths
-import './config/database';
 import 'api/index';
